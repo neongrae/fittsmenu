c10777b7b7f26152dd516e600b69ed1044e21eee
Patrick Schlafer <me@pschlafer.com>
2013-08-20 20:00:53 -0400

    Added Base JS Functions
    • 'Main.js' added to html page & folder
    • Position of .fittsmenu needs refinement. Currently, the entire size
    of this element is affecting where it displays in relation to the
    actual click. The gray 'X' may need to created as an actual element
    instead of added with ':before' in CSS.
    • Has NOT been tested on mobile, but would expect it to work with at
    least basic first touch (not on touchend).
    • Animation of .fittsmenu works on opening, but not on close.

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	// For testing, remove the default 'active' class
+	$('.fittsmenu').removeClass('active');
+
+	var width = $(window).width(),
+		height = $(window).height();
+
+	$(document).bind('touchstart click', function(e){
+
+		if(!$('.fittsmenu').hasClass('active')){
+		
+		var Xpos = e.pageX,
+			 Ypos = e.pageY;
+
+		// var Xrel = (Xpos/width)*100,
+			 // Yrel	= (Ypos/width)*100;
+
+		console.log('x: '+Xpos);
+		console.log('Y: '+Ypos);
+
+		// console.log('xRel: '+Xrel);
+		// console.log('YRel: '+Yrel);
+
+		var fittHeight = $('.fittsmenu').height(),
+			fittWidth	= $('.fittsmenu').width();
+
+		$('.fittsmenu').css({ "position":"absolute", "top":Ypos-(fittHeight), "left":Xpos-( (fittWidth-80)/2) });
+		$('.fittsmenu').addClass('active');
+
+		}else{
+			$('.fittsmenu').removeClass('active');
+			$('.fittsmenu').css({"top":"", "left":""});
+		}//end 
+
+	})//doc.click
+
+
+})
